534ccc047b8503cbd93c6145eed1fd82237397c2
Kabir Samra <kabir@Kabirs-MacBook-Pro.local>
2020-04-21 18:39:48 -0700

    basic calendar view, needs calender, itinerary, and event time UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Home from './components/Home'
-import Login from './components/Login'
-import Register from './components/Register'
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Help from './components/Help';
+import CalendarCreator from './components/CalendarCreator';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
@@ -21,8 +23,14 @@ export default function App() {
           <Route path="/register">
             <Register />
           </Route>
+          <Route path="/help">
+            <Help />
+          </Route>
           <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/calendarCreator">
+            <CalendarCreator />
           </Route>
         </Switch>
     </Router>
--- src/components/Help.js
+++ src/components/Help.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {Container} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 
-export default function Register() {
+export default function Help() {
+/*    const onClick = useCallback(){
+
+        <div className="login">
+            ASKDFJAD
+        </div>
+    );*/
+
+    const onSubmitHandler = {
+        // direct guest to check their email for further instructions
+        // button for back to login page
+        // https://stackoverflow.com/questions/50644976/react-button-onclick-redirect-page/50645395
+        // import { useHistory } from 'react-router-dom';
+        // const App = () => {
+        //    const history = useHistory()
+        //    <i className="icon list arrow left"
+        //       onClick={() => {
+        //         history.goBack()
+        //    }}></i>
+        // }
+    }
+
     return (
         <Container>
             <div className="login">
@@ -18,24 +39,8 @@ export default function Register() {
                     <input className="login-info" required={true} id="email"/>
                 </div>
 
-                <div className="login-input-label">
-                    <label htmlFor="username">Username &nbsp;</label>
-                </div>
-
-                <div className="login-input">
-                    <input className="login-info" required={true} id="username"/>
-                </div>
-
-                <div className="login-input-label">
-                    <label htmlFor="password">Password &nbsp;</label>
-                </div>
-
-                <div className="login-input">
-                    <input className="login-info" required={true} id="password"/>
-                </div>
-
                 <div className="login-buttons">
-                    <Button onClick={event =>  window.location.href='/home'}size="lg" variant="primary">Sign up!</Button>{' '}
+                    <Button /*onClick={onClick}*/ size="lg" variant="primary">Submit</Button>{' '}
                     <br/>
                 </div>
                 <div className="back-to-login">
