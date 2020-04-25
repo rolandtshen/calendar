6d852dca5dbed15e4baa52413de465f8398de62b
Sakura Rapolu <rapolusakura@gmail.com>
2020-04-25 11:49:25 -0700

    sends email to one person

--- src/components/SendEmails.js
+++ src/components/SendEmails.js
@@ -7,23 +7,14 @@ import emailjs from 'emailjs-com';
 var SERVICE_ID = 'default_service'; //'cs201_group_proj'; 
 var TEMPLATE_ID = 'invitemember'; 
 var USER_ID = 'user_VGneIDigKJxiIiIxj92z6'; 
+emailjs.init("USER_ID");
 
 function SendEmails(emails) {
     console.log(emails); 
-
     var templateParams = {
         eventName: 'gang gang',
         eventLink: 'Check this out!'
     };
-
-    // emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, templateParams, USER_ID)
-    //   .then((result) => {
-    //       console.log(result.text);
-    //   }, (error) => {
-    //       console.log(error.text);
-    //   });
-
-    emailjs.init("user_VGneIDigKJxiIiIxj92z6");
 
     emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams)
     .then(function(){ 
@@ -32,16 +23,6 @@ function SendEmails(emails) {
        alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
     });
 
-    // const data = {
-    //   from: 'FUCK <rapolu@usc.edu>',
-    //   to: 'rapolu@usc.edu',
-    //   subject: 'Hello',
-    //   text: 'Testing some Mailgun awesomeness!'
-    // };
-
-    // mailgun.messages().send(data, (error, body) => {
-    //   console.log(body);
-    // });
 }
 
 export default SendEmails; 
